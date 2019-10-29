0e17c6765bcd07949df8e63dd35cf3e5941c73fe
Eduarda <meduardagalindo8@gmail.com>
2019-10-29 12:11:38 -0300

    Inserida a função do thermo para calcular a eos e alterada as unidades dos dados de entrada. Além disso o código foi meio que reformulado quando percebi que algumas coisas não seriam uteis.

--- Teste_Estabilidade.py
+++ Teste_Estabilidade.py
@@ -5,25 +5,32 @@ import thermo
 import matplotlib.pyplot as plt
 
 ## Encontrar os pontos estacionarios. Estes correspondem aos pontos nos quais a derivada de g com respeito a Y é 0
+
+
 class fugacity:
     def coefficientsPR(w,Bin,R,Tc,Pc,T,P,Nc):
         a = np.zeros(Nc)
         b = np.zeros(Nc)
         K = np.zeros(Nc)
-        aalpha_i = np.zeros(Nc)
-        aalpha_ij = np.zeros((Nc,Nc))
+        aalpha_i = np.zeros(Nc); aalpha_ij= np.zeros((Nc,Nc))
+
 
         for i in range(0,Nc):
-            k = 0.37464 + 1.5422*w[i] -0.26992*w[i]**2; #kappa - PR eos
+            k = 0.37464 + 1.54226*w[i] -0.26992*w[i]**2; #kappa - PR eos
             alpha = (1+k*(1-(T/Tc[i])**(1/2)))**2; #coef - PR eos
-            aalpha_i[i] = 0.45724*R**2*Tc[i]**2*alpha/Pc[i]; #coef - PR eos
+            aalpha_i[i] = 0.45724*(R*Tc[i])**2*alpha/Pc[i]; #coef - PR eos
             b[i] = 0.07780*R*Tc[i]/Pc[i]; #coef - PR eos
-            K[i] = math.exp(5.37*(1+w[i])*(1-1/(T/Tc[i])))/(P/Pc[i]); #acho que isso encontro em algum livro
-        return K,b,aalpha_ij,aalpha_i
+            K[i] = math.exp(5.37*(1+w[i])*(1-1/(T/Tc[i])))/(P/Pc[i]); #
+
+        for i in range(0,Nc):
+            for j in range(0,Nc):
+                aalpha_ij[i,j] = (aalpha_i[i]*aalpha_i[j])**(1/2) *(1.
+                - Bin[i,j]); # aalphaij
+        return K,b,aalpha_ij
 
 
 
-    def V_PR(bm,R,T,P,A,aalpha,ph):
+    '''def V_PR(bm,R,T,P,A,aalpha,ph):
         ## Acredito que aqui ele tenha usado a EOS de PR da equação cúbica pra resolver, identificando se vai ter uma,duas ou tres raízes.
         # Não sei de onde ele tirou essa forma de escrever, mas pretendo testar com afunção do thermo que já retorna Vl e Vv
 
@@ -53,10 +60,8 @@ class fugacity:
             roots=2
         else:
             b1 = (-a2/2 + (a2**2/4 + a1**3/27)**(1/2))**(1/3)
-            b22 =  -a2/2 - (a2**2/4 + a1**3/27)**(1/2)
-            if b22 < 0: aux = -(-b22)*(1/3)
-            else: aux = b22**(1/3)
-            b2 = np.sign(( -a2/2 - (a2**2/4 + a1**3/27)**(1/2)))*abs(aux)
+            b2 = np.sign(( -a2/2 - (a2**2/4 + a1**3/27)**(1/2)))*abs(-a2/2 - (a2**2/4 + a1**3/27)**(1/2))**(1/3)
+
             if ph==1:
                Vl = b1 + b2 - p/3
                Vv = 1e-20
@@ -65,102 +70,112 @@ class fugacity:
                 Vl = 1e-20
             roots=1
 
-        return Vl,Vv
+        return Vl,Vv'''
 
 
-    def lnphi(x,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_i,aalpha_ij,b,ph): #ph stands for phase (1-liquid, 2-vapor)
+    def lnphi(x,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_ij,b):
         lnphi = np.zeros(Nc)
         lnphi_termo = np.zeros(Nc)
 
-        aalpha = 0; bm = 0
+        aalpha = 0; bm = 0;
+        bm = sum(x*b)
+
         for i in range(0,Nc):
-            bm = bm + x[i]*b[i]
             for j in range(0,Nc):
-                aalpha_ij[i,j] = (aalpha_i[i]*aalpha_i[j])**(1/2) * (1-Bin[i,j]); # aalphaij
                 aalpha = aalpha + x[i]*x[j]*aalpha_ij[i,j]
+
         B = bm*P/(R*T)
         A = aalpha*P/(R*T)**2
 
-        #Teste do cálculo do volume da fase:
-        '''CHECAR SE O MÉTODO DO THERMO TA BATENDO COM O QUE O DR. Schmall ESCREVEU'''
-        eos = thermo.eos_mix.PRMIX(Tc,Pc,w,x,Bin,T=T,P=P)
-        Vl_t = eos.V_l
-        #Vv_t = eos.V_g
+        eos = thermo.eos_mix.PRMIX(Tcs=Tc,Pcs=Pc,omegas=w,zs=x,kijs=Bin,T=T,P=P)
+        if eos.phase == 'l': V = eos.V_l
+        else: V = eos.V_g
 
-        Vl,Vv = fugacity.V_PR(bm,R,T,P,A,aalpha,ph)
+        Z = P*V/(R*T)
 
-        Zv = P*Vv/(R*T)
-        Zl = P*Vl/(R*T)
         for i in range(0,Nc):
             psi_i = 0
             for j in range(0,Nc):
                 psi_i = psi_i + x[j]*aalpha_ij[i,j]
-            if ph == 1:
-                lnphi[i] = b[i]/bm*(Zl-1) - np.log(Zl - B) - \
-                    A/(2*(2**(1/2))*B)*(2*psi_i/aalpha -\
-                    b[i]/bm)*np.log((Zl+(1+2**(1/2))*B)/(Zl+(1-2**(1/2))*B))
-            else:
-                    lnphi[i] = b[i]/bm*(Zv-1) - np.log(Zv - bm*P/(R*T)) -\
-                    A/(2*(2**(1/2))*B)*(2*psi_i/aalpha -\
-                    b[i]/bm)*np.log((Zv+(1+2**(1/2))*B)/(Zv+(1-2**(1/2))*B))
+
+            lnphi[i] = b[i]/bm*(Z-1) - np.log(Z - B) - \
+                A/(2*2**(1/2)*B)*(2*psi_i/aalpha -\
+                b[i]/bm)*np.log((Z+(1+2**(1/2))*B)/(Z+(1-2**(1/2))*B))
+
         return lnphi
 
 
 
 class StabilityTest:
     def Stability(w,Bin,R,Tc,Pc,T,P,Nc,z):
-        K,b,aalpha_ij,aalpha_i = fugacity.coefficientsPR(w,Bin,R,Tc,Pc,T,P,Nc)
-        #****************************Initial guess******************************#
-        ## Both approaches bellow should be used in case the phase is in the critical region
-        ''' VER QUAL CRITÉRIO RETORNA SE É ESTÁVEL OU NÃO'''
-        ## Used alone when the phase investigated is liquid like (ph == 1)
-        Y = z/K
-        Y_old = 0.9*Y #just to pass the first while below
-        y = Y/sum(Y)
+        K,b,aalpha_ij = fugacity.coefficientsPR(w,Bin,R,Tc,Pc,T,P,Nc)
+    #****************************Initial guess******************************#
+    ## Both approaches bellow should be used in case the phase is in the critical region
 
-        while max(abs(Y/Y_old - 1))>1e-9: #convergência - Y_old/Y termo a termo
-            Y_old = Y
-            lnphiz = fugacity.lnphi(z,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_i,aalpha_ij,b,1); #by the PR EOS
-            lnphiy = fugacity.lnphi(y,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_i,aalpha_ij,b,2);
-            for i in range(len(Y)):
-                Y[i] = math.exp( math.log(z[i]) + lnphiz[i] - lnphiy[i] )
-            y = Y/sum(Y);
+        #identifying the initial phase 'z' mole fraction
+        eos = thermo.eos_mix.PRMIX(Tcs=Tc,Pcs=Pc,omegas=w,zs=z,kijs=Bin,T=T,P=P)
 
-        if sum(Y) <= 1: return 'estavel'
-        else: return'instavel'
+        lnphiz = fugacity.lnphi(z,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_ij,b)
+        ## Used alone when the phase investigated is clearly liquid like (ph == 1)
+        if eos.phase == 'l' or eos.phase == 'l/g':
+            Y = z/K
+            Y_old = 0.9*Y #just to pass the first while below
+            y = Y/sum(Y)
+            while max(abs(Y/Y_old - 1))>1e-9: #convergência - Y_old/Y termo a termo
+                Y_old = Y
 
-        ## Used alone when the phase investigated is vapour like (ph == 2)
-        Y = K*z
-        Y_old = 0.9*Y
-        y = np.divide(Y,sum(Y)) #ta dando erro
+                lnphiy = fugacity.lnphi(y,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_ij,b)
+                for i in range(len(Y)):
+                    Y[i] = math.exp( math.log(z[i]) + lnphiz[i] - lnphiy[i] )
+                y = Y/sum(Y);
 
-        while max(abs(Y/Y_old - 1))>1e-9:
-            Y_old = Y
-            lnphiz = fugacity.lnphi(z,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_i,aalpha_ij,b,2);
-            lnphiy = fugacity.lnphi(y,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_i,aalpha_ij,b,1);
-            for i in range(len(Y)):
-                Y[i] = math.exp( math.log(z[i]) + lnphiz[i] - lnphiy[i] )
-            y = np.divide(Y,sum(Y))
+            if sum(Y) <= 1: print('estavel')#estavel2 = 1
+            else: print('instavel') #estavel2 = 0
 
-        if sum(Y) <= 1: return 'estavel'
-        else: return'instavel'
+    ## Used alone when the phase investigated is clearly vapour like (ph == 2)
+        if eos.phase == 'g' or eos.phase == 'l/g':
+            Y = K*z
+            Y_old = 0.9*Y
+            y = np.divide(Y,sum(Y)) #ta dando erro
 
+            while max(abs(Y/Y_old - 1))>1e-9:
+                Y_old = Y
+                lnphiy = fugacity.lnphi(y,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_ij,b);
+                for i in range(len(Y)):
+                    Y[i] = math.exp( math.log(z[i]) + lnphiz[i] - lnphiy[i] )
+                y = np.divide(Y,sum(Y))
 
-    def TPD(Nc,T,P,R,Tc,Pc,Bin,w):
-        #*************************Tangent Plane distance plot******************#
-        t = np.linspace(0.01,0.99,0.9/0.02) #vetor auxiliar
-        print(t)
+            if sum(Y) <= 1: print('estavel')#estavel2 = 1
+            else: print('instavel') #estavel2 = 0
+
+        '''
+        if estavel1==1 and estavel2==1: return 'estavel'
+        else: return 'instavel'''
+
+    def TPD(Nc,T,P,R,Tc,Pc,Bin,w,z):
+        K,b,aalpha_ij = fugacity.coefficientsPR(w,Bin,R,Tc,Pc,T,P,Nc)
+        #**********************Tangent Plane distance plot*********************#
+        t = np.linspace(0.01,0.99,0.9/0.002) #vetor auxiliar
         TPD = np.zeros(len(t)) ##F
         for i in range(0,len(t)):
             aux = 0;
-            lnphiz = StabilityTest.lnphi(z,Nc,T,P,R,Tc,Pc,Bin,w,1) #original phase
-            x = np.array([1-t[i],t[i]]) #new phase composition (1-t e t) - apenas válido para Nc=2 acredito eu
-            lnphix = StabilityTest.lnphi(x,Nc,T,P,R,Tc,Pc,Bin,w,2) #new phase (vapor- ph=2)
+            lnphiz = fugacity.lnphi(z,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_ij,b) #original phase
+            x = np.array([1-t[i],t[i]]) #new phase composition (1-t e t) - apenas válido para Nc=2 acredito eu.
+            '''O modo que x varia implica no formato de TPD. No presente exemplo,
+            a fração molar do segundo componente de x varia direto com t, que é a
+            variável de plotagem. Logo, a distancia dos planos tangentes será
+            zeros em z[1]. O contrário ocorreria'''
+            lnphix = fugacity.lnphi(x,Nc,T,P,R,Tc,Pc,Bin,w,aalpha_ij,b); #new phase (vapor- ph=2)
             for j in range(0,Nc):
                 fix = math.exp(lnphix[j])*x[j]*P
-                fiz = math.exp(lnphiz[j])*z[j]*P; #se acaba dividindo os dois embaixo, não tem pra quê multiplicar pela pressão -ps: realmente nem sei o que a pressão ta fazendo aí
+                fiz = math.exp(lnphiz[j])*z[j]*P;
                 aux = aux + x[j]*R*T*(math.log(fix/fiz))
             TPD[i] = aux
+
+        for i in range(len(t)):
+            if abs(TPD[i] - K[1]) < 0.1:
+                print(TPD[i])
+                print('ysp',t[i])
 
         plt.figure(0)
         plt.plot(t,TPD)
--- Testes/Testes.py
+++ Testes/Testes.py
@@ -3,19 +3,20 @@ from ..Teste_Estabilidade import StabilityTest
 import unittest
 
 class estabilidade(unittest.TestCase):
-
+#Unidades estão atualmente no SI
     def test_caso2(self):
         z = np.array([0.2,0.8]) #exemplo aleatório
         caso = 2
 
         if caso == 2:
             Nc = 2;
-            P = (100 * 10E5 / 101325) * 14.7;
-            R = 10.73159;
-            T = 350 *(9/5);
-            Tc = (9/5)*np.array([190.6, 460.4]);
-            Pc = 14.7*np.array([45.4,33.4]);
+            P = (100)*10E5 #* 10E5 / 101325) * 14.7; pressão de 100bar e ele converte para atm e depois psi
+            R = 8.3144598 #10.73159; - unid inglesa
+            T = 350 + 273#*(9/5); - T em C originalmente - faltou ele somar 32
+            Tc = np.array([190.6, 460.4]) + 273; #(9/5)*
+            Pc =np.array([45.4,33.4])*10E5; # 14.7*
             w = np.array([0.008,0.227]);
             Bin = np.array([[0,0.0236],[0.0236,0]]);
 
-        estavel = StabilityTest.Stability(w,Bin,R,Tc,Pc,T,P,Nc,z)
+        StabilityTest.Stability(w,Bin,R,Tc,Pc,T,P,Nc,z)
+        StabilityTest.TPD(Nc,T,P,R,Tc,Pc,Bin,w,z)

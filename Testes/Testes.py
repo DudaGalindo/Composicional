fe3bb5ad384075179de9a7bc993f597a5f3b3349
Eduarda <meduardagalindo8@gmail.com>
2020-01-08 19:14:44 -0300

    Testing just one test

--- Testes/Testes.py
+++ Testes/Testes.py
@@ -36,7 +36,7 @@ class estabilidade(unittest.TestCase):
         print('L: ',obj.L[1],'V: ',obj.V[1])
         print('fl: ',obj.fl,'fv: ',obj.fv)
 
-    def test_caso2(self):
+    '''def test_caso2(self):
         z = np.array([0.5,0.5]) #exemplo aleatório
         P = (100*1E5)#/101325)*14.7# pressão de 100bar e ele converte para atm e depois psi
         T = 350#*9/5 #- T em K to R
@@ -50,14 +50,14 @@ class estabilidade(unittest.TestCase):
         obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
         sp1,sp2 = obj.Stability(z)
         if sp1>1 or sp2>1:
-            obj.molar_properties(z,Mw)
+            obj.molar_properties(z,Mw)'''
 
-        '''if sp1<1 and sp2<1:
+    '''if sp1<1 and sp2<1:
             TPD = obj.TPD(z)
             if TPD.any()<0: #checar se isso iria funcionar
                 obj.molar_properties(z,Mw)'''
 
-        print('x: ',obj.x,'y: ',obj.y)
+    '''print('x: ',obj.x,'y: ',obj.y)
         print('K: ',obj.K)
         print('L: ',obj.L[1],'V: ',obj.V[1])
-        print('fl: ',obj.fl,'fv: ',obj.fv)
+        print('fl: ',obj.fl,'fv: ',obj.fv)'''

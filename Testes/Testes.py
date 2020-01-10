ae15059a1b228d1b29c60173e26de272402dc5b2
DudaGalindo <meduardagalindo8@gmail.com>
2020-01-10 04:19:25 -0300

    Add tests from Li et al paper - and confirmed the results

--- Testes/Testes.py
+++ Testes/Testes.py
@@ -3,12 +3,136 @@ from ..Checa_Estabilidade import StabilityTest
 import thermo
 import unittest
 
-#R = 8.3144598
 R = 10.73159
-class estabilidade(unittest.TestCase):
+Tc = np.array([-116.59, 305.69, 453.65, 652.01])
+Tc = (Tc + 459.67)
+Pc = np.array([667.2,551.1,430.59,305.68])
+w = np.array([0.008,0.193,0.296,0.49])
+Mw = np.array([16.043,58.124,86.16,142.29])
+P = 600
+T = 160 + 459.67
+Bin = np.zeros([len(w),len(w)])
+C7 = np.array([0,0,0,1])
 
+class testes_Li_et_al_table4(unittest.TestCase):
+
+    #Units: English system
+    # methane n-butane n-hexane n-decane
+    def test5(self):
+        z = np.array([-0.58,0.38,0.6,0.6])
+        print('\nTable4 Test 5:')
+
+        obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
+        obj.molar_properties(z,Mw)
+
+        obj.x = np.round(obj.x,4)
+        obj.y = np.round(obj.y,4)
+        obj.K = np.round(obj.K,4)
+
+        x = [0.1676,0.2239,0.3088,0.2997]
+        y = [0.9115,0.0686,0.0190,0.0009]
+        K = [5.4393,0.3062,0.0616,0.0031]
+
+        self.assertEqual([obj.x[0],obj.x[1],obj.x[2],obj.x[3]],x,'ValueError:Failed')
+        self.assertEqual([obj.y[0],obj.y[1],obj.y[2],obj.y[3]],y,'ValueError:Failed')
+        self.assertEqual([obj.K[0],obj.K[1],obj.K[2],obj.K[3]],K,'ValueError:Failed')
+
+    def test4(self):
+        z = np.array([0,0,0,1])
+
+        print('\nTable4 Test 4:')
+
+        obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
+
+        obj.molar_properties(z,Mw)
+        # sp1,sp2 = obj.Stability(z)
+        # if sp1>1 or sp2>1:
+        #     obj.molar_properties(z,Mw)
+        obj.x = np.round(obj.x,4)
+        obj.y = np.round(obj.y,4)
+        obj.K = np.round(obj.K,4)
+
+        x = [0.1724,0.0000,0.0000,0.8276]
+        y = [0.9980,0.0000,0.0000,0.0020]
+        K = [5.7895,0.2867,0.0538,0.0024]
+
+        self.assertEqual([obj.x[0],obj.x[1],obj.x[2],obj.x[3]],x,'ValueError:Failed')
+        self.assertEqual([obj.y[0],obj.y[1],obj.y[2],obj.y[3]],y,'ValueError:Failed')
+        self.assertEqual([obj.K[0],obj.K[3]],[K[0],K[3]],'ValueError:Failed')
+
+    def test3(self):
+        z = np.array([0,0,1,0])
+
+        print('\nTable4 Test 3:')
+
+        obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
+
+        obj.molar_properties(z,Mw)
+        # sp1,sp2 = obj.Stability(z)
+        # if sp1>1 or sp2>1:
+        #     obj.molar_properties(z,Mw)
+        obj.x = np.round(obj.x,4)
+        obj.y = np.round(obj.y,4)
+        obj.K = np.round(obj.K,4)
+
+        x = [0.1789,0.0000,0.8211,0.0000]
+        y = [0.9507,0.0000,0.0493,0.0000]
+        K = [5.3130,0.2999,0.0600,0.0030]
+
+        self.assertEqual([obj.x[0],obj.x[1],obj.x[2],obj.x[3]],x,'ValueError:Failed')
+        self.assertEqual([obj.y[0],obj.y[1],obj.y[2],obj.y[3]],y,'ValueError:Failed')
+        self.assertEqual([obj.K[0],obj.K[2]],[K[0],K[2]],'ValueError:Failed')
+
+    def test2(self):
+        z = np.array([0,1,0,0])
+
+        print('\nTable4 Test 2:')
+
+        obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
+
+        obj.molar_properties(z,Mw)
+        # sp1,sp2 = obj.Stability(z)
+        # if sp1>1 or sp2>1:
+        #     obj.molar_properties(z,Mw)
+        obj.x = np.round(obj.x,4)
+        obj.y = np.round(obj.y,4)
+        obj.K = np.round(obj.K,4)
+
+        x = [0.1639,0.8361,0.0000,0.0000]
+        y = [0.6960,0.3040,0.0000,0.0000]
+        K = [4.2473,0.3636,0.0905,0.0066]
+
+        self.assertEqual([obj.x[0],obj.x[1],obj.x[2],obj.x[3]],x,'ValueError:Failed')
+        self.assertEqual([obj.y[0],obj.y[1],obj.y[2],obj.y[3]],y,'ValueError:Failed')
+        self.assertEqual([obj.K[0],obj.K[1]],[K[0],K[1]],'ValueError:Failed')
+
+    def test1(self):
+        z = np.array([1,0,0,0])
+
+        print('\nTable4 Test 3:')
+
+        obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
+
+        obj.molar_properties(z,Mw)
+        # sp1,sp2 = obj.Stability(z)
+        # if sp1>1 or sp2>1:
+        #     obj.molar_properties(z,Mw)
+        obj.x = np.round(obj.x,4)
+        obj.y = np.round(obj.y,4)
+        obj.K = np.round(obj.K,4)
+
+        x = [0.1724,0.0000,0.0000,0.8276]
+        y = [0.9980,0.0000,0.0000,0.0020]
+        K = [5.7895,0.2867,0.0538,0.0024]
+
+        self.assertEqual([obj.x[0],obj.x[1],obj.x[2],obj.x[3]],x,'ValueError:Failed')
+        self.assertEqual([obj.y[0],obj.y[1],obj.y[2],obj.y[3]],y,'ValueError:Failed')
+        self.assertEqual([obj.K[0],obj.K[3]],[K[0],K[3]],'ValueError:Failed')
+
+class testes_casos_Schmall(unittest.TestCase):
     def test_caso1(self):
         #Units: T[Rankine];P[psia]
+        R = 10.73159
         T = (200 + 459.67) #Rankine*5/9=Kelvin
         P = 160 #psi*6894.7573 = Pa
         Tc = np.array([-116.32,206.24,305.96,454.1,660.38])
@@ -27,19 +151,13 @@ class estabilidade(unittest.TestCase):
         if sp1>1 or sp2>1:
             obj.molar_properties(z,Mw)
 
-        #checar se isso abaixo iria funcionar e se é necessário.
-        #Acho que só vou saber disso se der erro sem.
-        '''if sp1<1 and sp2<1:
-            TPD = obj.TPD(z)
-            if TPD.any()<0:
-                obj.molar_properties(z,Mw)'''
-
         print('x: ',obj.x,'y: ',obj.y)
         print('K: ',obj.K)
         print('L: ',obj.L[1],'V: ',obj.V[1])
         print('fl: ',obj.fl,'fv: ',obj.fv)
 
-    def test_book(self):
+    def test_bookDandekar(self):
+        R = 10.73159
         z = np.array([0.8232,0.0871,0.0505,0.0198,0.0194])
         Tc = np.array([343,666,845,1112,1291])#Rankine
         Pc = np.array([667.2,615.8,489.4,305.7,205.7]) #psia
@@ -50,7 +168,7 @@ class estabilidade(unittest.TestCase):
                         [0.08,0.039,0.022,0.004,0]])
         w = np.array([0.008,0.152,0.251,0.490,0.742])
         Mw = np.array([16.043,44.097,72.15,142.29,226.41])
-        T = 100+460#Fahrenheit
+        T = 100+459.67#Fahrenheit
         P = 1500 #psia
         C7 = np.array([0,0,0,1,1])
 
@@ -65,10 +183,34 @@ class estabilidade(unittest.TestCase):
         print('K: ',obj.K)
         print('L: ',obj.L[1],'V: ',obj.V[1])
         print('fl: ',obj.fl,'fv: ',obj.fv)
-    def test_book2(self):
-        z = np.array([0.5,0.3,0.2])
-        
-    '''def test_caso2(self):
+
+    def test_Abbot(self):
+        #units: SI
+        #elements: methane ethane propane
+        R = 8.3144598
+        z = np.array([0.1,0.2,0.7])
+        Tc = np.array([190.55,305.33,369.522])
+        Pc = np.array([45.95, 48.72, 42.49])
+        P = 13.8
+        T = 283.15
+        Mw = np.array([16.04,30.07,44.1])
+        C7 = 0
+        w = np.array([0.011,0.099,0.153])
+        Bin = np.zeros([3,3])
+        print('\nExemplo 10.5:')
+
+        obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
+        sp1,sp2 = obj.Stability(z)
+        if sp1>1 or sp2>1:
+            obj.molar_properties(z,Mw)
+
+        print('x: ',obj.x,'y: ',obj.y)
+        print('K: ',obj.K)
+        print('L: ',obj.L[1],'V: ',obj.V[1])
+        print('fl: ',obj.fl,'fv: ',obj.fv)
+
+    def test_caso2(self):
+        R = 8.3144598
         Bin = np.array([[0, 0.033],[0.033, 0]])
         Tc = np.array([369.8, 425.2])*9/5
         Pc = 14.7*np.array([41.9, 37.5])
@@ -84,12 +226,14 @@ class estabilidade(unittest.TestCase):
         sp1,sp2 = obj.Stability(z)
         if sp1>1 or sp2>1:
             obj.molar_properties(z,Mw)
+
         print('x: ',obj.x,'y: ',obj.y)
         print('K: ',obj.K)
         print('L: ',obj.L[1],'V: ',obj.V[1])
         print('fl: ',obj.fl,'fv: ',obj.fv)
 
     def test_caso3(self):
+        R = 8.3144598
         z = np.array([0.5,0.5]) #exemplo aleatório
         P = (100*1E5)/101325*14.7# pressão de 100bar e ele converte para atm e depois psi
         T = 350*9/5 #- T em K to R
@@ -104,14 +248,14 @@ class estabilidade(unittest.TestCase):
         obj = StabilityTest(w,Bin,R,Tc,Pc,T,P,C7)
         sp1,sp2 = obj.Stability(z)
         if sp1>1 or sp2>1:
-            obj.molar_properties(z,Mw)'''
+            obj.molar_properties(z,Mw)
 
-    '''if sp1<1 and sp2<1:
+        '''if sp1<1 and sp2<1:
             TPD = obj.TPD(z)
             if TPD.any()<0: #checar se isso iria funcionar
                 obj.molar_properties(z,Mw)'''
 
-    '''    print('x: ',obj.x,'y: ',obj.y)
+        print('x: ',obj.x,'y: ',obj.y)
         print('K: ',obj.K)
         print('L: ',obj.L[1],'V: ',obj.V[1])
-        print('fl: ',obj.fl,'fv: ',obj.fv)'''
+        print('fl: ',obj.fl,'fv: ',obj.fv)

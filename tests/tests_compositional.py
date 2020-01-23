f1bb2bfb18d74268488e3f8acc004ee41cd8760c
Eduarda <meduardagalindo8@gmail.com>
2020-01-23 16:32:24 -0300

    Added a test for a fluid in a single phase

--- tests/tests_compositional.py
+++ tests/tests_compositional.py
@@ -119,18 +119,42 @@ class testes_Li_et_al_table4(unittest.TestCase):
                 self.assertAlmostEqual(obj.K[i],K[i],4,'ValueError:Failed')
 
 class testes_casos_Schmall(unittest.TestCase):
-    # def teste_caso5(self):
-    #     R = 8.3144598
-    #     z = np.array([1.]) #exemplo aleatório
-    #     P = np.array([13.79e6])
-    #     T = np.array([366.33])
-    #     Tc = np.array([617.7])
-    #     Pc =np.array([2103000.0])
-    #     w = np.array([0.4885])
-    #     Bin = np.array([0])
-    #     Mw = np.array([142.28e-3],dtype = np.double)
-    #     C7 = np.array([0])
-    #     print('\ncaso5:')
+    def teste_caso5(self):
+        R = 8.3144598
+        z = np.array([1.]) #exemplo aleatório
+        P = np.array([13.79e6])
+        T = np.array([366.33])
+        Tc = np.array([617.7])
+        Pc =np.array([2103000.0])
+        w = np.array([0.4884])
+        Bin = np.array([0])
+        Mw = np.array([142.28e-3],dtype = np.double)
+        C7 = np.array([0])
+        print('\ncaso5:')
+
+        obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
+        obj.run(z,Mw)
+
+        print('x: ',obj.x,'y: ',obj.y)
+        print('K: ',obj.K)
+        print('L: ',obj.L,'V: ',obj.V)
+        print('fl: ',obj.fl,'fv: ',obj.fv)
+
+    # def test_caso1(self):
+    #     #Units: T[Rankine];P[psia]
+    #     R = 10.73159
+    #     T = (200 + 459.67) #Rankine*5/9=Kelvin
+    #     P = 160 #psi*6894.7573 = Pa
+    #     Tc = np.array([-116.32,206.24,305.96,454.1,660.38])
+    #     Tc = (Tc + 459.67)
+    #     Pc = np.array([667.19386,615.75821,551.09625,477.028914,367.5444663])#*6894.7573
+    #     w  = np.array([0.008,0.152,0.193,0.27504,0.443774])
+    #     Mw = np.array([16.043,44.097,58.124,86,134]) #molecular weight
+    #     Bin  = np.array([[0,0,0,0.01,0.02], [0,0,0,0.01,0.01], [0,0,0,0,0.01], [0.01,0.01,0,0,0.01], [0.02,0.01,0.01,0.01,0]])
+    #     z = np.array([0.6,0.1,0.1,0.1,0.1])
+    #     C7 = 0
+    #
+    #     print('\ncaso1:')
     #
     #     obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
     #     obj.run(z,Mw)
@@ -139,73 +163,49 @@ class testes_casos_Schmall(unittest.TestCase):
     #     print('K: ',obj.K)
     #     print('L: ',obj.L,'V: ',obj.V)
     #     print('fl: ',obj.fl,'fv: ',obj.fv)
-
-    def test_caso1(self):
-        #Units: T[Rankine];P[psia]
-        R = 10.73159
-        T = (200 + 459.67) #Rankine*5/9=Kelvin
-        P = 160 #psi*6894.7573 = Pa
-        Tc = np.array([-116.32,206.24,305.96,454.1,660.38])
-        Tc = (Tc + 459.67)
-        Pc = np.array([667.19386,615.75821,551.09625,477.028914,367.5444663])#*6894.7573
-        w  = np.array([0.008,0.152,0.193,0.27504,0.443774])
-        Mw = np.array([16.043,44.097,58.124,86,134]) #molecular weight
-        Bin  = np.array([[0,0,0,0.01,0.02], [0,0,0,0.01,0.01], [0,0,0,0,0.01], [0.01,0.01,0,0,0.01], [0.02,0.01,0.01,0.01,0]])
-        z = np.array([0.6,0.1,0.1,0.1,0.1])
-        C7 = 0
-
-        print('\ncaso1:')
-
-        obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
-        obj.run(z,Mw)
-
-        print('x: ',obj.x,'y: ',obj.y)
-        print('K: ',obj.K)
-        print('L: ',obj.L,'V: ',obj.V)
-        print('fl: ',obj.fl,'fv: ',obj.fv)
-
-    def test_caso2(self):
-        R = 8.3144598
-        Bin = np.array([[0, 0.033],[0.033, 0]])
-        Tc = np.array([369.8, 425.2])*9/5
-        Pc = 14.7*np.array([41.9, 37.5])
-        w = np.array([0.152, 0.193])
-        Mw = np.array([44.097, 58.124])
-        z = np.array([0.5, 0.5])
-        T = 9/5*396
-        P = 14.7*(3.86e6/101325)
-        C7 = 0
-
-        print('\ncaso2:')
-        obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
-        obj.run(z,Mw)
-
-        print('x: ',obj.x,'y: ',obj.y)
-        print('K: ',obj.K)
-        print('L: ',obj.L,'V: ',obj.V)
-        print('fl: ',obj.fl,'fv: ',obj.fv)
-
-    def test_caso3(self):
-        R = 8.3144598
-        z = np.array([0.5,0.5]) #exemplo aleatório
-        P = (100*1E5)/101325*14.7# pressão de 100bar e ele converte para atm e depois psi
-        T = 350*9/5 #- T em K to R
-        Tc = np.array([190.6, 460.4])*9/5;
-        Pc =np.array([45.4,33.4])*14.7; # 14.7*
-        w = np.array([0.008,0.227])
-        Bin = np.array([[0,0.0236],[0.0236,0]])
-        Mw = np.array([44.097,58.124])
-        C7 = 0
-
-        print('\ncaso3:')
-        obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
-        obj.run(z,Mw)
-
-        print('x: ',obj.x,'y: ',obj.y)
-        print('K: ',obj.K)
-        print('L: ',obj.L,'V: ',obj.V)
-        print('fl: ',obj.fl,'fv: ',obj.fv)
-
+    #
+    # def test_caso2(self):
+    #     R = 8.3144598
+    #     Bin = np.array([[0, 0.033],[0.033, 0]])
+    #     Tc = np.array([369.8, 425.2])*9/5
+    #     Pc = 14.7*np.array([41.9, 37.5])
+    #     w = np.array([0.152, 0.193])
+    #     Mw = np.array([44.097, 58.124])
+    #     z = np.array([0.5, 0.5])
+    #     T = 9/5*396
+    #     P = 14.7*(3.86e6/101325)
+    #     C7 = 0
+    #
+    #     print('\ncaso2:')
+    #     obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
+    #     obj.run(z,Mw)
+    #
+    #     print('x: ',obj.x,'y: ',obj.y)
+    #     print('K: ',obj.K)
+    #     print('L: ',obj.L,'V: ',obj.V)
+    #     print('fl: ',obj.fl,'fv: ',obj.fv)
+    #
+    # def test_caso3(self):
+    #     R = 8.3144598
+    #     z = np.array([0.5,0.5]) #exemplo aleatório
+    #     P = (100*1E5)/101325*14.7# pressão de 100bar e ele converte para atm e depois psi
+    #     T = 350*9/5 #- T em K to R
+    #     Tc = np.array([190.6, 460.4])*9/5;
+    #     Pc =np.array([45.4,33.4])*14.7; # 14.7*
+    #     w = np.array([0.008,0.227])
+    #     Bin = np.array([[0,0.0236],[0.0236,0]])
+    #     Mw = np.array([44.097,58.124])
+    #     C7 = 0
+    #
+    #     print('\ncaso3:')
+    #     obj = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
+    #     obj.run(z,Mw)
+    #
+    #     print('x: ',obj.x,'y: ',obj.y)
+    #     print('K: ',obj.K)
+    #     print('L: ',obj.L,'V: ',obj.V)
+    #     print('fl: ',obj.fl,'fv: ',obj.fv)
+    #
 
     # def test_bookDandekar(self):
     #     R = 10.73159
